2c1f6e590af05e3b92756f37c1bb3c86d02a4b40
Mourcat <vybornovs@rambler.ru>
2021-11-27 16:11:10 +0300

    Telegram Bot(aiogram)

--- create_bot.py
+++ create_bot.py
@@ -8,5 +8,5 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 storage = MemoryStorage()
 
 
-bot = Bot(token=os.getenv('TOKEN'))
+bot = Bot(token='2125360952:AAGK98quCuKWWk0v86bOw38vDSLeoM8vklY')
 dp = Dispatcher(bot, storage=storage)
